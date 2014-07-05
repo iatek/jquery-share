80bf04c07299b7de337a4cc6b4d33e0971095a69
Ben <ben@ros.io>
2014-07-05 14:30:49 -0600

    Modify .pop click binding
    Use a delegate instead of an eventhandler, so creation/deletion of shares won't affect it.  Also use evt.preventDefault instead of return false so other delegates can have access to the event.

--- jquery.share.js
+++ jquery.share.js
@@ -84,9 +84,9 @@
                     }
                     
                     // bind click
-                    $('.pop').click(function(){
+                    $(document).on('click', '.pop', function(evt){
                         window.open($(this).attr('href'),'t','toolbar=0,resizable=1,status=0,width=640,height=528');
-                        return false;
+                        evt.preventDefault();
                     });
                     
                     
